5fa2dce0857ab6421cc6b930e6c62032473abcb6
Agacy junior <juniormartinssom@gmail.com>
2022-04-28 13:27:20 -0300

    Next link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
+
 function Header() {
   return (
     <header>
@@ -26,7 +29,18 @@ function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   )
 }
