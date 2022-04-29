aef549f5662176d2d0e3553123ca32504a093532
Agacy junior <juniormartinssom@gmail.com>
2022-04-29 12:10:18 -0300

    Install recoil

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
 
 import { Movie } from '../typings'
 import requests from '../utils/requests'
@@ -28,6 +29,11 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth();
+  // const [showModal, setShowModal] = useState(false);
+
+  if (loading) return null
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -47,7 +53,8 @@ const Home = ({
           <Row title="Romance Movies" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
         </section>  
-      </main>     
+      </main> 
+
     </div>
   )
 }
